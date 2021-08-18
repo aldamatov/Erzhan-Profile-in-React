6ad039fdfe184595f1e2f50046d04effdceb451c
Erzhan Aldamatov <aldamatov@gmail.com>
2021-08-18 12:43:02 +1000

    added navbar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { Hero } from './component/Hero';
 function App() {
   return (
     <div>
-      Hello 
-
+    
       <Hero/>
     </div>
   );
---
+++ src/component/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {TopNavBar} from "./top-navbar";
+
+export const Hero = () =>{
+
+    return <div className="hero">
+        
+        <TopNavBar/>
+        </div>
+}
